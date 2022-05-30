e55872bd4dedf90059b09be20dea9725f603de4f
harish velmurugan <vharish2001@gmail.com>
2022-05-30 11:34:09 +0530

    completed backend

--- image_caption/caption_generator/models.py
+++ image_caption/caption_generator/models.py
@@ -1,3 +1,11 @@
+from tkinter import image_names
 from django.db import models
-
+import os
 # Create your models here.
+# models.py
+
+
+class Caption(models.Model):
+
+    Img = models.ImageField(upload_to='images/')
+
